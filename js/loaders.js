a666d29a450664005f17c9eb9f6d44fc9afadeac
Erika Bracamonte <erikab2731@gmail.com>
2019-04-14 21:06:17 +0200

    video5

--- js/loaders.js
+++ js/loaders.js
@@ -1,3 +1,7 @@
+import Level from './Level.js';
+import {createBackgroundLayer, createSpriteLayer} from './layers.js';
+import {loadBackgroundSprites} from './sprites.js';
+
 export function loadImage(url) {
     return new Promise(resolve => {
         const image = new Image();
@@ -8,7 +12,16 @@ export function loadImage(url) {
     });
 }
 
-export function loadLevel(name) {
-    return fetch(`/levels/${name}.json`)
-    .then(r => r.json());
+function createTiles(level, backgrounds) {
+    backgrounds.forEach(background => {
+        background.ranges.forEach(([x1, x2, y1, y2]) => {
+            for (let x = x1; x < x2; ++x) {
+                for (let y = y1; y < y2; ++y) {
+                    level.tiles.set(x, y, {
+                        name: background.tile,
+                    });
+                }
+            }
+        });
+    });
 }
