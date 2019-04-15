3c146491f8c9f7547e9dff5efd30c051a33598b3
Erika Bracamonte <erikab2731@gmail.com>
2019-04-15 14:32:21 +0200

    video6

--- js/entities.js
+++ js/entities.js
@@ -1,4 +1,5 @@
 import Entity from './Entity.js';
+import Go from './traits/Go.js';
 import Jump from './traits/Jump.js';
 import Velocity from './traits/Velocity.js';
 import {loadMarioSprite} from './sprites.js';
@@ -9,14 +10,14 @@ export function createMario() {
         const mario = new Entity();
         mario.size.set(14, 16);
 
-        mario.addTrait(new Velocity());
+        mario.addTrait(new Go());
         mario.addTrait(new Jump());
         //mario.addTrait(new Velocity());
 
         mario.draw = function drawMario(context) {
-            sprite.draw('idle', context, this.pos.x, this.pos.y);
+            sprite.draw('idle', context, 0, 0);
         }
-        
+
         return mario;
     });
 }
